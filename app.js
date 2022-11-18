d395412c0acf621e6e58632a4dc89d05cce62333
Yves Boulinguez <yves.boulinguez@grow-your-business.fr>
2022-11-18 13:47:12 +0100

    feat: added arrow function and comments

--- app.js
+++ app.js
@@ -3,25 +3,29 @@ const rate = document.querySelector('#rate');
 const labelRate = document.querySelector('#label-rate')
 const form = document.querySelector('form');
 const resultTTC = document.querySelector('.result');
-
 let result;
 
-function getRate(element, labelElement){
+/**
+ * Functin to get rate value
+ */
+const getRate = (element, labelElement) => {
     element.addEventListener('input', () => {
-        labelElement.textContent = `Taux de TVA : ${element.value}` 
+        labelElement.textContent = `Taux de TVA : ${element.value}%`
     })
 }
 
-getRate(rate,labelRate)
+getRate(rate, labelRate)
 
+/**
+ * Function to get result TTC price
+ */
 const calcultTva = (numberHT, rate) => {
-    result = Number(((numberHT * rate) / 100 ) + numberHT);
-    console.log(result, 'resultat');
-    resultTTC.textContent = `Prix TTC : ${result}%`
+    result = Number(((numberHT * rate) / 100) + numberHT);
+    resultTTC.textContent = `Prix TTC : ${result}€`
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    calcultTva(Number(priceHT.value), Number(rate.value) )
+    calcultTva(Number(priceHT.value), Number(rate.value))
 })
 
